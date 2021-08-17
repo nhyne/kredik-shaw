60e32b66a0cde185d69ac304df5f9e068782a181
nhyne <nhyne@nhyne.dev>
2021-08-16 23:32:54 -0400

    moving PREnv service and creating docker build

---
+++ github-watcher/src/main/scala/actions/DockerImageBuild.scala
@@ -0,0 +1,24 @@
+package actions
+
+import zio.process.{Command, CommandError}
+import zio.{Has, ZLayer}
+
+import java.io.File
+
+object DockerImageBuild {
+
+  type DockerBuildService = Has[Service]
+
+  val live: ZLayer[Any, Nothing, DockerBuildService] = ZLayer.succeed(
+    new Service {
+      override def buildDockerImage(file: File, workingDir: File): Unit = ???
+
+      override def buildDockerImage(command: Seq[String], env: Map[String, String]): Unit = ???
+    }
+  )
+
+  trait Service {
+    def buildDockerImage(file: File, workingDir: File)
+    def buildDockerImage(command: Seq[String], env: Map[String, String])
+  }
+}
