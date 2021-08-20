96277bfbf640c3402566c7493b8b133381b7c8a4
nhyne <nhyne@nhyne.dev>
2021-08-20 15:14:26 -0400

    cleaning up main program

--- github-watcher/src/main/scala/Main.scala
+++ github-watcher/src/main/scala/Main.scala
@@ -1,5 +1,4 @@
 import zio._
-import zio.console.putStrLn
 import zio.blocking.Blocking
 import zio.console.Console
 import zhttp.service.EventLoopGroup
@@ -13,33 +12,13 @@ import GithubApi.{zioHttpClient, Http4sClient, GithubClient}
 
 object Main extends App {
 
-  val program = for {
-    repos <- GithubApi.listRepos()
-    _ <- ZIO.foreach_(repos)(repo => putStrLn(repo.name))
-    clt <- ZIO.service[zioHttpClient.Service]
-    zioTopics <- clt.getTopics("zio", "zio")
-    _ <- ZIO.foreach_(zioTopics.names)(topic => putStrLn(topic))
-    /*
-      check the merge code, if we have a success code then we continue to reading the config file -- stored where?
-      if we have a failure then we want to comment on the PR saying so -- or do we send a response saying "merge failed"
-     */
-
-    _ <- WebhookApi.createPRNamespace(23, "zio")
-    _ <- WebhookApi.server.start
-  } yield ()
+  private val program = WebhookApi.server.start
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
 
-    val httpLayer = Http4sClient.live
-    val githubClientLayer = GithubClient.live(None)
-    val asyncHttpClientZioBackend = ZLayer.fromManaged(AsyncHttpClientZioBackend.managed())
-
     program.inject(
-      httpLayer,
-      githubClientLayer,
       Console.live,
       Blocking.live,
-      asyncHttpClientZioBackend >>> zioHttpClient.live,
       k8sDefault,
       Namespaces.live,
       ServerChannelFactory.auto,
