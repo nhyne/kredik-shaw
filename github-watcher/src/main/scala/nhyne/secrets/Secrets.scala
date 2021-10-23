97a6164acf90d7bfc7698edd78c5f9ecaea74faa
nhyne <nhyne@nhyne.dev>
2021-10-23 14:49:38 -0400

    some config work for secrets

--- github-watcher/src/main/scala/nhyne/secrets/Secrets.scala
+++ github-watcher/src/main/scala/nhyne/secrets/Secrets.scala
@@ -1,16 +1,13 @@
 package nhyne.secrets
 
 import nhyne.git.GitEvents.Repository
-import zio.{ Has, ULayer, ZIO, ZLayer }
+import zio.{ Has, URLayer, ZIO, ZLayer }
 import io.github.vigoo.zioaws.secretsmanager.SecretsManager
 import io.github.vigoo.zioaws.secretsmanager.model.GetSecretValueRequest
 import nhyne.Errors.KredikError
-import nhyne.config.ApplicationConfig
+import nhyne.config.{ ApplicationConfig, SecretsConfig }
 
 trait Secrets {
-  def generateAndSaveSecret(
-    repository: Repository
-  ): ZIO[SecretsManager with Has[ApplicationConfig], KredikError, Unit]
   def readSecret(
     repository: Repository
   ): ZIO[SecretsManager with Has[ApplicationConfig], KredikError, String]
@@ -18,26 +15,30 @@ trait Secrets {
 }
 
 object Secrets {
-  val live: ULayer[Has[Secrets]] = ZLayer.succeed(new Secrets {
-    override def generateAndSaveSecret(
-      repository: Repository
-    ): ZIO[SecretsManager with Has[ApplicationConfig], KredikError, Unit] = ???
+  def live: URLayer[Has[ApplicationConfig], Has[Secrets]] =
+    ZIO.service[ApplicationConfig].map(_.webhookSecrets).map {
+      case SecretsConfig.SecretsManagerConfig(prefix) => secretsManager(prefix)
+      case SecretsConfig.SecretsEnvConfig(prefix)     => ???
+    }
 
-    // TODO: Reading this should depend on what kind of secret config we have
-    //    Or should we return different versions of this for the different secret config types we have?
-    override def readSecret(
-      repository: Repository
-    ): ZIO[SecretsManager with Has[ApplicationConfig], KredikError, String] =
-      (for {
-        secretPrefix <- ZIO
-                          .service[ApplicationConfig]
-                          .map(_.webhookSecrets.prefix())
-        sm           <- ZIO.service[SecretsManager.Service]
-        readSecret   <- sm.getSecretValue(
+  def secretsManager(prefix: String) =
+    ZLayer.succeed(new Secrets {
+
+      private val secretPrefix = prefix
+
+      // TODO: Reading this should depend on what kind of secret config we have
+      //    Or should we return different versions of this for the different secret config types we have?
+      override def readSecret(
+        repository: Repository
+      ): ZIO[SecretsManager with Has[ApplicationConfig], KredikError, String] =
+        (for {
+          sm         <- ZIO.service[SecretsManager.Service]
+          readSecret <- sm.getSecretValue(
                           GetSecretValueRequest(s"$secretPrefix/${repository.fullName}")
                         )
-        secret       <- readSecret.secretString
-      } yield secret).mapError(e => KredikError.GeneralError(e.toThrowable))
+          secret     <- readSecret.secretString
+        } yield secret).mapError(e => KredikError.GeneralError(e.toThrowable))
+    })
 
-  })
+  def environmentSecrets(prefix: String) = ???
 }
